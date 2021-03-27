d30cad49f859689894c7bb05b60da105ba35cbc1
Leozin89Python <leozin069@gmail.com>
2021-03-26 21:18:10 -0300

    sds2

--- ws-sds2/dsdeliver/frontend/web/front-web/src/orders/orderLocation.tsx
+++ ws-sds2/dsdeliver/frontend/web/front-web/src/orders/orderLocation.tsx
@@ -1,4 +1,56 @@
+import { useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import AsyncSelect from 'react-select'
+import { fetchLocalMapBox } from '../api'
+
+const initialPosition = {
+    lat :51.505, 
+    lng :-0.09
+}
+
+type Place = {
+    label?:string,
+    value?:string,
+    position :{
+        lat :number,
+        lng :number
+    }
+}
+
 function OrderLocation(){
+    const [address, setAddress] = useState<Place>({
+        position :initialPosition
+    })
+    
+    const loadOptions = async (inputValue: string, callback: (places: Place[]) => void) => {
+        const response = await fetchLocalMapBox(inputValue);
+      
+        const places = response.data.features.map((item: any) => {
+          return ({
+            label: item.place_name,
+            value: item.place_name,
+            position: {
+              lat: item.center[1],
+              lng: item.center[0]
+            },
+            place: item.place_name,
+          });
+        });
+      
+        callback(places);
+      };
+      
+      const handleChangeSelect = (place: Place) => {
+        setAddress(place);
+       /* onChangeLocation({
+          latitude: place.position.lat,
+          longitude: place.position.lng,
+          address: place.label!
+        }); */
+      };
+
+
+
     return(
         <div className="order-location-container">
            <div className="order-location-content">
@@ -6,8 +58,27 @@ function OrderLocation(){
                     Selecione onde o pedidio deve ser entregue:
                 </h3>
                 <div className="filter-container">
-                    
+                    <AsyncSelect 
+                        placeholder="Digite um endereço para a entrega do pedido"
+                        className="filter"
+                        loadOptions={loadOptions}
+                        onChange={value => handleChangeSelect(value as Place)}
+                    />
                 </div>
+                <MapContainer 
+                    center={address.position}
+                    zoom={13} 
+                    scrollWheelZoom>
+                    <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={address.position}>
+                    <Popup>
+                        my marker
+                    </Popup>
+                    </Marker>
+                </MapContainer>
            </div>
         </div>
     )
